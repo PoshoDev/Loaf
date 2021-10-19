ac1af70c33871cabecc727b24929d20bc773778e
Rodrigo <green.tie.rgm@gmail.com>
2021-10-18 21:18:53 -0500

    idk

--- Loaf.py
+++ Loaf.py
@@ -1,19 +1,28 @@
-import pymysql, datetime
-import _creds
+import pymysql, datetime, socket
+
+host = socket.gethostbyname(socket.gethostname)
+port = 80 # Default XAMPP Apache server port.
+user = "root"
+pasw = ""
+db = None
+creds = ""
+
+def bake(host=host, port=port, user=user, pasw=pasw, db=db, creds=creds):
+    return "lol"
 
 # Easier on the eyes.
-def Call(func, *args):
+def call(func, *args):
     call = "CALL " + func + "("
     if len(args) > 0:
         for i in range(len(args)):
-            call += (str(args[i]) if type(args[i])==type(1) else ParseNull(args[i])) + (", " if i<len(args)-1 else ");")
+            call += (str(args[i]) if type(args[i])==type(1) else parseNull(args[i])) + (", " if i<len(args)-1 else ");")
     else: call += ");"
-    q = Query(call)
+    q = query(call)
     return q[0][0] if len(q)==1 else q
 
 # A query.
-def Query(query):
-    conn = pymysql.connect(host=_creds.host, port=_creds.port, user=_creds.user, passwd=_creds.pasw, db=_creds.data)
+def query(query):
+    conn = pymysql.connect(host=host, port=port, user=user, passwd=pasw, db=db)
     conn_object = conn.cursor()
     conn_object.execute(query)
     str = conn_object.fetchall()
@@ -21,13 +30,13 @@ def Query(query):
     conn.close()
     return str
 
-def GetToday():
+def getToday():
     dat = datetime.date.today() + datetime.timedelta(days=1)
     return dat.strftime("%Y-%m-%d")
 
-def GetTomorrow():
+def getTomorrow():
     dat = datetime.date.today() + datetime.timedelta(days=1)
     return dat.strftime("%Y-%m-%d")
 
-def ParseNull(val):
+def parseNull(val):
     return "NULL" if val in ["", "NULL"] else "'"+val+"'"
