e727b7a38ff6c3dbe714a6b7bba55a9321a974c3
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2021-10-19 03:05:07 -0500

    Quickfix.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 DESCRIPTION = 'Effortlessly access your MySQL server and procedures, plus some other utilities!'
 with open('README.md', encoding="utf8") as f:
     LONG_DESCRIPTION = f.read()
