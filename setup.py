e1fb942a8ad97d329ac15c8f814f3aa3bbf6e249
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2022-05-08 17:44:23 -0500

    Dict fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.23"
+VERSION = "0.1.29"
 DESCRIPTION = "Effortlessly access your SQL servers and procedures, plus " \
               "some other utilities!"
 with open('README.md', encoding="utf8") as f:
