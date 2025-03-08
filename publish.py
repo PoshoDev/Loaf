c938d4f7ec1b5bab9391defb186764e81f09cfba
PoshoDev <green.tie.rgm@gmail.com>
2025-03-08 16:16:52 -0600

    use new publish method

--- publish.py
+++ publish.py
@@ -4,6 +4,12 @@ import json
 import shutil
 from rich import print
 
+# Remove the build and dist folders.
+if os.path.exists("build"):
+    shutil.rmtree("build")
+if os.path.exists("dist"):
+    shutil.rmtree("dist")
+
 # Get the PyPI credentials.
 creds = "pypi_creds.json"
 if os.path.exists(creds):
@@ -12,16 +18,18 @@ if os.path.exists(creds):
 else:
     print(f"[blink red]File '{creds}' does not exist!")
     sys.exit()
-username = data["username"]
-password = data["password"]
+token = data["token"]
+
 # Build the package.
 try:
     os.system("python setup.py sdist bdist_wheel")
 except Exception as e:
     print(f"[blink red]Error building package: {e}")
     sys.exit()
+
 # Upload the package.
-os.system(f"twine upload --username {username} --password {password} dist/*")
+os.system(f"twine upload dist/* -u __token__ -p {token}")
+
 # Move the contents of the dist folder to "_Ignore/Builds".
 dir_dest = "_Ignore/Builds"
 dir_src = "dist"
@@ -31,6 +39,7 @@ if os.path.exists(dir_dest):
 else:
     print(f"[blink red]Directory '{dir_dest}' does not exist!")
     sys.exit()
+
 # Remove the build and dist folders.
 shutil.rmtree("build")
 shutil.rmtree("dist")
